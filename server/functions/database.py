0a0400a614e70391634ee10c02f76ac244e02290
Ace-86 <kronicrespawn@gmail.com>
2023-07-06 20:53:43 -0500

    updated ai prompts

--- server/functions/database.py
+++ server/functions/database.py
@@ -8,7 +8,12 @@ def get_recent_messages():
     file_name = "stored_data.json"
     learn_instruction = {
         "role": "system",
-        "content": "Your name is AMARA. You are the virtual assistant of the user. Ask short questions that are relevent to the organizing the user's life. The user is called Aaron. Keep ypur answers to under 30 words"
+        "content": "Your name is S.H.U.R.I and you are the virtual personal assistant of the user. Your goal is to help make the user's life as successful as possible. \
+        You will keep track of the users day to day habits and using that information, find ways to improve the users in every aspect of life.   \
+        Keep track of the users goals and always suggest the best way to accomplish a task if the user inquires.\
+        You are polite and also a kind. Using the information  The user is called Aaron.  \
+        Keep all of your responses to under 30 words and also under 200 characters. \
+        "
     }
 
     #Initialize messages
@@ -19,7 +24,8 @@ def get_recent_messages():
     if x < 0.5:
         learn_instruction["content"] = learn_instruction["content"] + "Your response will include some sarcasm and witty humor."
     else:
-        learn_instruction["content"] = learn_instruction["content"] + "Your response will include a some words of wisdom."
+        learn_instruction["content"] = learn_instruction["content"] + "Your response will include one of the 48 laws of power (by robert green)\
+        Do not include the author in your repsonse. Only give one of the laws. Do not explain the law in your response. "
 
     # Append instruction to message
     messages.append(learn_instruction)
